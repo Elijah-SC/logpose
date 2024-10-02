f20314840d8ca19a4fdf7a07bd6bb36b6a6bc463
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-10-01 20:01:47 -0600

    project initialized

---
+++ client/src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import HereMap from '@/components/HereMap.vue';
+</script>
+
+<template>
+  <h1>Test</h1>
+</template>
+
+<style scoped lang="scss">
+  .map {
+    width: 9rem;
+    height: 9rem; 
+  }
+</style>
