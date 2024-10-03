8b91fdc859aed65a0fa5c7fb334d75fbdf5bd4d9
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-10-03 14:41:03 -0600

    home page done however changes could be made.

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,36 +1,33 @@
 <script setup>
 import Login from './Login.vue';
-
 </script>
 
 <template>
-  <header>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3">
-      <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-        <img class="ms-2" src="/img/logpose-logo.svg" alt="logo" />
-      </router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav ms-auto">
-          <li>
-            <router-link :to="{ name: 'About' }" class="btn text-light lighten-30 selectable">
-              About
-            </router-link>
-          </li>
-          <li class="mx-3">
-            <router-link :to="{ name: 'Explore' }" class="btn text-light lighten-30 selectable">
-              Explore
-            </router-link>
-          </li>
-        </ul>
-        <!-- LOGIN COMPONENT HERE -->
-        <Login />
-      </div>
-    </nav>
-  </header>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3 sticky-top">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <img class="ms-2" src="/img/logpose-logo.svg" alt="logo" />
+    </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav ms-auto">
+        <li>
+          <router-link :to="{ name: 'About' }" class="btn text-light lighten-30 selectable">
+            About
+          </router-link>
+        </li>
+        <li class="mx-3">
+          <router-link :to="{ name: 'Explore' }" class="btn text-light lighten-30 selectable">
+            Explore
+          </router-link>
+        </li>
+      </ul>
+      <!-- LOGIN COMPONENT HERE -->
+      <Login />
+    </div>
+  </nav>
 </template>
 
 <style scoped>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -14,7 +14,7 @@ import HereMap from '@/components/HereMap.vue';
     </div>
   </section>
 
-  <section class="container-fluid d-flex justify-content-center align-items-center">
+  <section class="container-fluid d-flex justify-content-center align-items-center about-section">
     <div class="row justify-content-center">
       <div class="col-9">
         <div class="row text-light">
@@ -32,12 +32,23 @@ import HereMap from '@/components/HereMap.vue';
                 Beatae officia facilis unde, doloremque, qui enim molestias quam facere quae vitae eum ullam.</p>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="">
-
+          <div class="col-md-6 h-100">
+            <div class="logpose-logo-piece-container">
+              <img class="logo-piece-img" src="/img/logpose-logo-piece.svg" alt="logo">
             </div>
-            <img class="logo-piece-img" src="/img/logpose-logo-piece.svg" alt="logo">
           </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="container-fluid explore-section d-flex justify-content-center align-items-center">
+    <div class="row">
+      <div class="col-12">
+        <div class="p-2 rounded bg-light explore-msg mb-3">
+          <h2>Adventure awaits you</h2>
+        </div>
+        <div class="text-center">
+          <button class="btn btn-light rounded-pill px-3" type="button">Explore</button>
         </div>
       </div>
     </div>
@@ -55,8 +66,17 @@ import HereMap from '@/components/HereMap.vue';
 section {
   &:not(.hero) {
     min-height: 100dvh;
-    background-color: #333;
   }
+}
+
+.about-section {
+  background-color: #333;
+}
+
+.explore-section {
+  background-image: url(https://images.unsplash.com/photo-1519289455504-3510c41b7cc3?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  background-size: cover;
+  background-position: center;
 }
 
 .about-msg-1 {
@@ -67,8 +87,19 @@ section {
   line-height: 2em;
 }
 
-.logo-piece-img {
+.explore-msg {
+  max-width: fit-content;
+}
+
+.logpose-logo-piece-container {
   height: 100%;
-  max-width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo-piece-img {
+  height: 250px;
+  aspect-ratio: 1/1;
 }
 </style>
