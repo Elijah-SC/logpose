8eff50dd37c67d16f5f5e089ebf117af160980f1
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-10-03 15:04:11 -0600

    navbar styling changes

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,23 +3,23 @@ import Login from './Login.vue';
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3 sticky-top">
+  <nav class="navbar navbar-expand-sm navbar-dark bg-white px-3 sticky-top">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <img class="ms-2" src="/img/logpose-logo.svg" alt="logo" />
     </router-link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+    <button class="navbar-toggler btn-outline-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
       aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav ms-auto">
         <li>
-          <router-link :to="{ name: 'About' }" class="btn text-light lighten-30 selectable">
+          <router-link :to="{ name: 'About' }" class="btn btn-outline-dark text-dark lighten-30 selectable">
             About
           </router-link>
         </li>
         <li class="mx-3">
-          <router-link :to="{ name: 'Explore' }" class="btn text-light lighten-30 selectable">
+          <router-link :to="{ name: 'Explore' }" class="btn btn-outline-dark text-dark lighten-30 selectable">
             Explore
           </router-link>
         </li>
@@ -33,7 +33,7 @@ import Login from './Login.vue';
 <style scoped>
 img {
   height: 45px;
-  background-color: azure;
+  background-color: rgb(255, 255, 255);
   border-radius: 10px;
   padding: 0 .6rem 0 .6rem;
 }
