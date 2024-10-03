4e60b1367bb9da55407e4d3a34ebfe746be3ac44
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-10-03 16:53:05 -0600

    quicksave3

--- server/src/services/SavedLocationsService.js
+++ server/src/services/SavedLocationsService.js
@@ -1,38 +1,50 @@
-import { dbContext } from "../db/DbContext.js"
-import { Forbidden } from "../utils/Errors.js"
+import { dbContext } from "../db/DbContext.js";
+import { Forbidden } from "../utils/Errors.js";
 
 class SavedLocationsService {
-    async deleteSavedLocation(locationId, userId) {
-        const deleteSavedLocation = await dbContext.SavedLocations.findById(locationId)
-        if (!deleteSavedLocation) throw new Error('invalid')
-        if (userId != deleteSavedLocation.creatorId) throw new Forbidden(`not authorized can not update this ${locationId}`)
-        await deleteSavedLocation.deleteOne()
-        return deleteSavedLocation
-    }
+  async deleteSavedLocation(locationId, userId) {
+    const deleteSavedLocation = await dbContext.SavedLocations.findById(
+      locationId
+    );
+    if (!deleteSavedLocation) throw new Error("invalid");
+    if (userId != deleteSavedLocation.creatorId)
+      throw new Forbidden(`not authorized can not update this ${locationId}`);
+    await deleteSavedLocation.deleteOne();
+    return deleteSavedLocation;
+  }
 
-    async updateSavedLocation(locationId, userInfo, savedlocationData) {
-        const savedLocation = await dbContext.SavedLocations.findById(locationId).populate('creator', 'picture name')
-        if (userInfo != savedlocationData.creatorId) throw new Forbidden(`not authorized can not update this ${locationId}`)
-        savedLocation.visited != savedlocationData.visited ?? savedLocation.visited
-        savedLocation.save()
-        return savedLocation
-    }
+  async updateSavedLocation(locationId, userInfo, savedlocationData) {
+    const savedLocation = await dbContext.SavedLocations.findById(
+      locationId
+    ).populate("creator", "picture name");
+    if (userInfo != savedlocationData.creatorId)
+      throw new Forbidden(`not authorized can not update this ${locationId}`);
+    savedLocation.visited = savedlocationData.visited ?? savedLocation.visited;
+    await savedLocation.save();
+    return savedLocation;
+  }
 
-    async getMySavedLocation(userId) {
-        const savedLocation = await dbContext.SavedLocations.find({ creatorId: userId }).populate('location')
-        return savedLocation
-    }
+  async getMySavedLocation(creatorId) {
+    const savedLocation = await dbContext.SavedLocations.find({
+      creatorId: creatorId,
+    }).populate("location");
+    return savedLocation;
+  }
 
-    async getLocationVisitor(locationId) {
-        const savedLocation = await dbContext.SavedLocations.find({ locationId: locationId }).populate('creator')
-        return savedLocation
-    }
+  async getLocationVisitor(locationId) {
+    const savedLocation = await dbContext.SavedLocations.find({
+      locationId: locationId,
+    }).populate("creator");
+    return savedLocation;
+  }
 
-    async createSavedLocation(savedLocationData) {
-        const savedLocation = await dbContext.SavedLocations.create(savedLocationData)
-        await savedLocation.populate('creator', 'picture name')
-        await savedLocation.populate('location')
-        return savedLocation
-    }
+  async createSavedLocation(savedLocationData) {
+    const savedLocation = await dbContext.SavedLocations.create(
+      savedLocationData
+    );
+    await savedLocation.populate("creator", "picture name");
+    await savedLocation.populate("location");
+    return savedLocation;
+  }
 }
-export const savedLocationService = new SavedLocationsService()
+export const savedLocationService = new SavedLocationsService();
