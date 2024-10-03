544b299e24c03b8cbafb83183adffaa8f65a0a90
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-10-03 15:26:22 -0600

    quicksave2

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -64,6 +64,9 @@ import HomeNavBar from "@/components/HomeNavBar.vue";
 <style scoped lang="scss">
 .hero {
   min-height: 90dvh;
+  background-image: url(https://images.unsplash.com/photo-1621178734002-ad899f4fec71?q=80&w=2129&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  background-size: cover;
+  background-position: center;
 }
 
 section {
