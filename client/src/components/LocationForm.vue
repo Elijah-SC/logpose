4cc5bf6545f1dcec9690e2e4e21746cd087d60bf
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-10-04 17:01:55 -0600

    styling location form

--- client/src/components/LocationForm.vue
+++ client/src/components/LocationForm.vue
@@ -3,9 +3,12 @@ import { ref } from 'vue';
 import HereMap from './HereMap.vue';
 
 const locationCategories = ['hiking', 'skiing', 'snowboarding', 'wilderness'];
-const editableFormData = ref({
+const locationData = ref({
   name: '',
   coverImg: '',
+  directions: '',
+  description: '',
+  category: '',
 
 })
 </script>
@@ -13,7 +16,7 @@ const editableFormData = ref({
 <template>
   <section class="container">
     <form class="row">
-      <div v-if="" class="col-12">
+      <div v-if="locationData.coverImg" class="col-12">
         <h4>Image Preview</h4>
       </div>
       <div class="col-md-6">
@@ -25,25 +28,29 @@ const editableFormData = ref({
         <input type="url" class="form-control" id="locationCoverImg" name="locaitonCoverImg">
       </div>
       <div class="col-12">
-        <label for="locationDirections">Directions</label>
+        <label class="form-label" for="locationDirections">Directions</label>
         <input class="form-control" id="locationDirections" name="locationDirections">
       </div>
       <div class="col-12">
-        <label for="locationDescription">Description</label>
-        <textarea name="locationDescription" id="locationDescription"></textarea>
+        <label class="form-label" for="locationDescription">Description</label>
+        <textarea class="form-control" name="locationDescription" id="locationDescription" rows="5"></textarea>
       </div>
       <div class="col-md-12">
-        <div class="d-flex">
-          <label for="locationCategories">Location Type</label>
-          <select class="form-control" name="locationCategories" id="locationCategories">
-            <option disabled selected value="">Select location category</option>
-            <option v-for="locationCategory in locationCategories" :key="locationCategory" :value="locationCategory">{{
-              locationCategory }}></option>
-          </select>
-        </div>
-        <div>
-          <label class="form-label" for="locationCheck"></label>
-          <input class="form-control" type="checkbox" name="locationCheck" id="locationCheck">
+        <div class="d-flex align-items-center">
+          <div class="w-75">
+            <label class="form-label" for="locationCategories">Location Type</label>
+            <select class="form-control" name="locationCategories" id="locationCategories">
+              <option disabled selected value="">Select location category</option>
+              <option v-for="locationCategory in locationCategories" :key="locationCategory" :value="locationCategory">
+                {{
+                  locationCategory }}</option>
+            </select>
+          </div>
+
+          <div class="w-25 mx-auto">
+            <label class="form-label" for="locationCheck">Use my location</label>
+            <input class="align-self-center" type="checkbox" name="locationCheck" id="locationCheck">
+          </div>
         </div>
       </div>
       <div class="col-md-12">
@@ -53,4 +60,8 @@ const editableFormData = ref({
   </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+textarea {
+  resize: none;
+}
+</style>
