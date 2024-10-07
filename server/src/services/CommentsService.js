27fe0a099a9cc7d1041f3030547b705ff23dacfa
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-10-07 14:47:28 -0600

    picture server complete and comment halfway

--- server/src/services/CommentsService.js
+++ server/src/services/CommentsService.js
@@ -1,3 +1,29 @@
-class CommentsService {}
+import { dbContext } from "../db/DbContext.js";
+import { Forbidden } from "../utils/Errors.js";
+
+class CommentsService {
+  async updateComment(userId, commentId, commentData) {
+    const updatedComment = await dbContext.Comments.findById(commentId);
+    if (updatedComment === null) {
+      throw new Error("There is no comment with id of", commentId);
+    } else if (updatedComment.creatorId !== userId) {
+      throw new Forbidden("You cannot update a comment that isn't yours");
+    }
+    updatedComment.body = commentData.body ?? updatedComment.body;
+    await updatedComment.save();
+    return updatedComment;
+  }
+  async createComment(commentData) {
+    const createdComment = await dbContext.Comments.create(commentData);
+    await createdComment.populate("creator", "-email -subs");
+    return createdComment;
+  }
+  async getLocationComments(locationId) {
+    const locationComments = await dbContext.Comments.find({
+      locationId: locationId,
+    }).populate("creator", "-email -subs");
+    return locationComments;
+  }
+}
 
 export const commentsService = new CommentsService();
