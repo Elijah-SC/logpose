5f524865e1741482d1c59b67fbd5af6c33fed6d9
Levania Isack <levaniaisack01@gmail.com>
2024-10-08 13:56:42 -0600

    added comment model

--- client/src/models/Comment.js
+++ client/src/models/Comment.js
@@ -1,7 +1,32 @@
+import { Account } from "./Account.js"
+
 export class Comment{
     constructor(data){
-
+        this.id = data.id
+        this.creatorId = new Account(data.creatorId)
+        this.body = data.body
+        this.updateAt = new Date(data.updateAt)
+        this.createAt = new Date(data.createAt)
     }
 }
 
-const data =
+// const data ={
+//     "locationId": "67045ab0aadbe059bd2777d8",
+//     "creatorId": "670066f19f345d9bf24778fd",
+//     "body": "comments",
+//     "_id": "670552a81f957143dbd17ded",
+//     "createdAt": "2024-10-08T15:41:28.010Z",
+//     "updatedAt": "2024-10-08T15:41:28.010Z",
+//     "__v": 0,
+//     "creator": {
+//         "_id": "670066f19f345d9bf24778fd",
+//         "name": "reyeci8440",
+//         "picture": "https://s.gravatar.com/avatar/3d385605e948ab730d01bfef9615ab4e?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fre.png",
+//         "points": 0,
+//         "createdAt": "2024-10-04T22:06:47.856Z",
+//         "updatedAt": "2024-10-04T22:06:47.856Z",
+//         "__v": 0,
+//         "id": "670066f19f345d9bf24778fd"
+//     },
+//     "id": "670552a81f957143dbd17ded"
+// }
