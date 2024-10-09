6fd4f22fb6e2ec9f9c243dc7e1c44bbe93f6492c
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-10-09 14:05:09 -0600

    slightly modified carousel in prep for pictures

--- client/src/components/Carousel.vue
+++ client/src/components/Carousel.vue
@@ -1,23 +1,13 @@
 <script setup>
 import { Location } from "@/models/Location.js";
 
-
 defineProps({
   location: { type: Location, required: true }
 })
 </script>
 
-
 <template>
-  <div id="locationCarouselIndicator" class="carousel slide">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#locationCarouselIndicator" data-bs-slide-to="0" class="active"
-        aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#locationCarouselIndicator" data-bs-slide-to="1"
-        aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#locationCarouselIndicator" data-bs-slide-to="2"
-        aria-label="Slide 3"></button>
-    </div>
+  <div id="locationCarouselIndicator" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img :src="location?.coverImg" class="d-block w-100" alt="...">
