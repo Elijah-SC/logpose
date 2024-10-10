d0c15871656c286877fe3e813d9089301bc75576
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-10-10 15:38:38 -0600

    done with svg pin

--- client/src/components/TrueHereMap.vue
+++ client/src/components/TrueHereMap.vue
@@ -64,11 +64,11 @@ function initializeMap() {
 }
 
 function initializeExploreMap(map) {
-  //   const svgIcon = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-  //     <circle cx="12" cy="12" r="10" fill="#0000FF" />
-  // </svg>`;
-
-  const svgMarkup = '<svg class="map-marker" height="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>map-marker</title><path  style="fill: red;" d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" /></svg>';
+  const svgMarkup = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50">
+  <circle cx="12" cy="10" r="9" fill="blue" opacity="0.7"/>
+  <circle cx="12" cy="10" r="4" fill="white"/>
+  <path d="M12 2A7 7 0 0 0 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9A7 7 0 0 0 12 2Z" fill="blue"/>
+</svg>`
 
   const icon = new H.map.Icon(svgMarkup);
 
